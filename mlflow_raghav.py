3944aaa667e93748bc6204a1555ea6fe531f0809
raghav2798 <44143915+raghav2798@users.noreply.github.com>
2025-11-04 18:44:25 +0000

    new change

--- mlflow_raghav.py
+++ mlflow_raghav.py
@@ -85,7 +85,7 @@ with mlflow.start_run():
     mlflow.log_artifact('RandomForestClassifier.pkl',"wineQuality_raghav")
     
 
-bekar_model=mlflow.pyfunc.load_model("runs:/edf06d9bb69748a2920e5a0f07d5e011/09baf419dfde490eb1ac0593e8adb0e1/artifacts/best_model/best_model.pkl")
+
 
 
 
