1cc4f96eeaab056894f5463ae2e741a6a3b955e2
ap1305 <66279680+ap1305@users.noreply.github.com>
2025-11-21 20:10:49 +0000

    Model Inference

--- model_Inferencing.py
+++ model_Inferencing.py
@@ -1,4 +1,15 @@
 import mlflow
+
 mlflow.set_tracking_uri("https://dagshub.com/ap1305/MLOps_E2E_1st.mlflow")
-import mlflow
-model=mlflow.pyfunc.load_model("mlflow-artifacts:/edf06d9bb69748a2920e5a0f07d5e011/09baf419dfde490eb1ac0593e8adb0e1/artifacts/best_model/best_model.pkl")
+
+local_path = mlflow.artifacts.download_artifacts(
+    artifact_uri="mlflow-artifacts:/edf06d9bb69748a2920e5a0f07d5e011/09baf419dfde490eb1ac0593e8adb0e1/artifacts/best_model/best_model.pkl"
+)
+
+print("Downloaded to:", local_path)
+import pickle
+
+with open(local_path, "rb") as f:
+    model = pickle.load(f)
+
+print(model.predic())
